6235a1a42269e64cdb87309d10c8b9ff54596019
SkolandHati <vlad.star.01@inbox.ru>
2023-11-21 23:39:10 +0300

    uodate UserData, add folder shared

---
+++ src/components/Shared-Components/v-input.vue
@@ -0,0 +1,89 @@
+<template>
+  <div id="block" class="block-input"
+       :class="{atherSetting: defaultSetting}"
+       v-for="(item, i) in labels">
+    <label class="label">{{item}}</label>
+    <input class="input" type="text"
+           v-model="state[i]"
+           :placeholder="item" required>
+      {{state.email}}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'v-input',
+    props:{
+      defaultSetting:{
+        type: Boolean
+      },
+      userDatas: {
+        type: Object,
+        required: true
+      }
+    },
+    setup(){
+      const labels = ['Логин', 'Имя', 'Фамилия', 'Email', 'Тел.Номер', 'Банк.Карта']
+      return {
+        labels,
+      }
+    },
+    created() {
+      this.login = this.userDatas.user_login || ''
+      this.first_name = this.userDatas.user_first_name || ''
+      this.last_name = this.userDatas.user_last_name || ''
+      this.email = this.userDatas.user_email || ''
+      this.number_phone = this.userDatas.user_number_phone || ''
+      this.cart_bank = this.userDatas.user_cart_bunk_number || ''
+    },
+    data(){
+      return{
+          login: '',
+          first_name: '',
+          last_name: '',
+          email: '',
+          number_phone:'',
+          cart_bank: ''
+      }
+    },
+    mounted() {
+      console.log(this.state[1])
+    },
+    methods:{
+
+    }
+  }
+
+</script>
+
+<style scoped>
+  .block-input{
+    display: flex;
+    width: 500px;
+    height: 50px;
+    margin-top: 17px;
+    margin-left: 150px;
+    border: 1.5px solid #6e6d6d;
+    border-radius: 7px;
+  }
+  .label{
+    display: block;
+    width: 100px;
+    font-size: 18px;
+    margin: 15px;
+  }
+  .input{
+    width: 60%;
+    height: 27px;
+    text-align: center;
+    margin-top: 10px;
+    margin-left: 25px;
+  }
+  form>.block-inputs>#block>#login{
+    margin-left: 45px;
+  }
+  form>.block-inputs>#block>.input{
+    margin-left: 10px;
+  }
+
+</style>
--- src/components/User/v-UserData.vue
+++ src/components/User/v-UserData.vue
@@ -2,56 +2,24 @@
     <form action="#" method="post" target="_blank">
       <h1 v-if="!defaultSetting">Заполните все поля и заказывайте товары в один клик!</h1>
       <fieldset class="block-inputs" :class="{atherSetting: defaultSetting}">
-        <div id="block" class="block-first-name" :class="{atherSetting: defaultSetting}">
-          <label id="label" for="login">Логин:*  </label>
-          <input class="input" type="text" name="login"
-                 :value="user.user_id !== undefined ? user.user_login: login || ''"
-                 placeholder="Логин" id="login" required>
-        </div>
-        <div id="block" class="block-first-name" :class="{atherSetting: defaultSetting}">
-          <label id="label" for="first_name">Имя:*  </label>
-          <input class="input" type="text" name="first_name"
-                 v-model="first_name"
-                 placeholder="Иван " id="first_name" required>
-        </div>
-        <div id="block" class="block-last-name" :class="{atherSetting: defaultSetting}">
-          <label id="label" for="last_name">Фамилия:*  </label>
-          <input class="input" type="text" name="last_name"
-                 v-model="last_name"
-                 placeholder="Иванов " id="last_name" required>
-        </div>
-        <div id="block" class="block-email" :class="{atherSetting: defaultSetting}">
-          <label id="label" for="email">E-mail:*  </label>
-          <input class="input" type="email" name="mail"
-                 placeholder="ivanov@gmail.com" id="email" required>
-        </div>
-        <div id="block" class="block-phone-number" :class="{atherSetting: defaultSetting}">
-          <label id="label" for="number">Тел.Номер:*  </label>
-          <input class="input" type="tel" name="numb"
-                 v-model="number_phone"
-                 placeholder="88005553535" id="number" required>
-        </div>
-        <div id="block" class="bank-number" :class="{atherSetting: defaultSetting}">
-          <label id="label" for="bank-number">Карта Банка:*  </label>
-          <input class="input" type="number" name="numb"
-                 v-model="cart_bank"
-                 placeholder="8800-5553-5355-3535" id="bank-number" required>
-        </div>
+        <inputModul :userDatas="user"/>
         <div v-if="!defaultSetting" class="submit-button">
-          <button type="submit">Сохранить данные</button>
+          <button type="submit" @click="submitUserInformation">Сохранить данные</button>
         </div>
       </fieldset>
-      {{login}}
     </form>
 </template>
 
 <script>
-
+  import inputModul from "@/components/Shared-Components/v-input.vue"
   import {mapGetters, mapActions} from "vuex";
-  import {reactive, ref} from "vue";
+  import {ref} from "vue";
 
   export default {
     name: 'v-UserData',
+    components:{
+      inputModul
+    },
     props:{
       defaultSetting:{
         type: Boolean
@@ -67,6 +35,73 @@
           us_cart_bank: null
       }
     },
+    setup(){
+
+      const login = ref('')
+      const first_name = ref('')
+      const last_name = ref('')
+      const email = ref('')
+      const number_phone = ref('')
+      const cart_bank = ref('')
+
+      const labels = ['Логин', 'Имя', 'Фамилия', 'Email', 'Тел.Номер', 'Банк.Карта']
+
+      const state = async () => {
+        try {
+          const states = {
+            'user_id': 'egwegew',
+            'login_user': login.value,
+            'first_user_name': first_name,
+            'last_user_name': last_name,
+            'email_user': email,
+            'number_phone_user': number_phone,
+            'cart_bank_user': cart_bank
+          }
+          console.log(states)
+          //await this.setterUserData(state_object)
+        }catch (e){
+          Promise.reject(e)
+        }
+      }
+
+      const submitUserInformation = async () => {
+        try {
+          const state_object = {
+            'user_id': await this.user.user_id,
+            'login_user': login,
+            'first_user_name': first_name,
+            'last_user_name': last_name,
+            'email_user': email,
+            'number_phone_user': number_phone,
+            'cart_bank_user': cart_bank
+          }
+          console.log(state_object)
+          //await this.setterUserData(state_object)
+        }catch (e){
+          Promise.reject(e)
+        }
+      }
+      return {
+        login,
+        first_name,
+        last_name,
+        email,
+        number_phone,
+        cart_bank,
+        state,
+        labels,
+        submitUserInformation
+      }
+
+    },
+    computed:{
+      ...mapGetters({
+        user: 'user/USERINSYSTEM'
+      })
+    },
+    mounted() {
+      this.loadData()
+    },
     methods:{
       ...mapActions({
         getUserData: 'user/getUser',
@@ -80,50 +115,6 @@
         }
       },
     },
-    computed:{
-      ...mapGetters({
-        user: 'user/USERINSYSTEM'
-      })
-    },
-    setup(){
-
-      const login = ref('')
-      const first_name = ref('')
-      const last_name = ref('')
-      const email = ref('')
-      const number_phone = ref('')
-      const cart_bank = ref('')
-
-      const submitUserInformation = async () => {
-        try {
-          const state_object = {
-            'user_id': await this.user.user_id,
-            'login_user': login,
-            'first_user_name': first_name,
-            'last_user_name': last_name,
-            'email_user': email,
-            'number_phone_user': number_phone,
-            'cart_bank_user': cart_bank
-          }
-          await this.setterUserData(state_object)
-        }catch (e){
-          Promise.reject(e)
-        }
-      }
-      return {
-        login,
-        first_name,
-        last_name,
-        email,
-        number_phone,
-        cart_bank
-      }
-
-    },
-    mounted() {
-      this.loadData()
-      console.log(this.login)
-    }
   }
 
 </script>
@@ -144,48 +135,8 @@
     height: 85%;
     border-radius: 5px;
   }
-  form>.block-inputs>#block{
-    width: 500px;
-    height: 50px;
-    margin-top: 17px;
-    margin-left: 150px;
-    border: 1.5px solid #6e6d6d;
-    border-radius: 7px;
-  }
-  form>.block-inputs>#block.atherSetting{
-    margin-left: 70px;
-  }
   form>.block-inputs.atherSetting{
     width: 100%;
-  }
-  form>.block-inputs>#block>#label{
-    font-size: 18px;
-    margin: 15px;
-  }
-  form>.block-inputs>#block>.input{
-    width: 60%;
-    height: 27px;
-    text-align: center;
-    margin-top: 10px;
-    margin-left: 25px;
-  }
-  form>.block-inputs>#block>#login{
-    margin-left: 45px;
-  }
-  form>.block-inputs>#block>#first_name{
-    margin-left: 60px;
-  }
-  form>.block-inputs>#block>#last_name{
-    margin-left: 22px;
-  }
-  form>.block-inputs>#block>#email{
-    margin-left: 43px;
-  }
-  form>.block-inputs>#block>#number{
-    margin-left: 10px;
-  }
-  form>.block-inputs>#block>#bank-number{
-    margin-left: 0px;
   }
   form>.block-inputs>.submit-button>button{
     margin: 10px;
